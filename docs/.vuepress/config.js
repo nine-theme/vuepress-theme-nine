9f7b6dfa30000b5cd876738735e1a80a6fb6e7c9
王佳 <wangjia_1919@163.com>
2019-02-03 16:59:17 +0800

    refactor

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+
+module.exports = {
+  title: 'vuepress-theme-nine',
+
+  description: 'nine\'s homepage',
+
+  theme: path.resolve(__dirname, '../../lib'),
+
+  themeConfig: {
+    personalInfo: {
+      nickname: 'nine',
+      description: 'Coder',
+      email: 'wangjia_1919@163.com',
+      location: 'sz',
+      organization: '',
+
+      avator: 'https://www.meteorlxy.cn/assets/img/avator.jpg',
+
+      sns: {
+        github: {
+          account: 'NineSwordsMonster',
+          link: 'https://github.com/NineSwordsMonster',
+        },
+      },
+    },
+
+    headerBackground: {
+      // url: '/assets/img/bg.jpg',
+
+      useGeo: true,
+    },
+
+    lastUpdated: true,
+
+    nav: [
+      { text: 'Home', link: '/', exact: true },
+      { text: 'Posts', link: '/posts/', exact: false },
+    ],
+  },
+}

964cdcd42b13f351655e9b745b20b35c39a53618
王佳 <wangjia_1919@163.com>
2019-03-07 12:27:27 -0600

    --amend

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,17 +1,40 @@
 <template>
-    <div class="main">
-        <Content/>
+    <div id="app">
+        <TheHeader/>
+
+        <TheMain :layout="$layout"/>
+
+        <TheFooter/>
     </div>
 </template>
 
 <script>
+  import nprogressMixin from '@theme/mixins/nprogress'
+  import TheHeader from '@theme/components/TheHeader'
+  import TheMain from '@theme/components/TheMain'
+  import TheFooter from '@theme/components/TheFooter'
   export default {
-    name: 'NineMainLayout'
+    name: 'Layout',
+    mixins: [
+      nprogressMixin
+    ],
+    components: {
+      TheHeader,
+      TheMain,
+      TheFooter
+    },
+    computed: {
+      $layout () {
+        const defaultLayout = 'post'
+        const layout = this.$page.frontmatter.layout || defaultLayout
+        return layout.toLowerCase()
+      },
+      $title () {
+        const isHome = this.$layout === 'home'
+        return isHome ? this.$siteTitle : this.$page.frontmatter.title + ' | ' + this.$siteTitle
+      }
+    }
   }
 </script>
 
-<style lang="less" scoped>
-    .main {
-        background: #d5f3ff;
-    }
-</style>
+<style lang="stylus" src="@theme/styles/theme.styl"/>

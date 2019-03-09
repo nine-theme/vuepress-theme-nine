2cdb7db6194b409083acfe5aebc29d26611b0f65
王佳 <wangjia_1919@163.com>
2019-03-09 12:33:28 -0600

    amend update

--- layouts/Posts.vue
+++ layouts/Posts.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="posts">
-    <PostsList/>
+    <PostsList class="main-div"/>
   </div>
 </template>
 
 <script>
-import PostsList from '../components/PostsList'
-export default {
-  name: 'Posts',
-  components: {
-    PostsList
+  import PostsList from '../components/PostsList'
+  export default {
+    name: 'Posts',
+    components: {
+      PostsList
+    }
   }
-}
 </script>

54601862bbe5f102d4fc3c5127487f55eb938137
王佳 <wangjia_1919@163.com>
2019-05-20 23:30:08 +0800

    cc init

--- enhanceApp.js
+++ enhanceApp.js
@@ -1,11 +1,59 @@
-import ElementUI from 'element-ui';
-import './styles/element.scss';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import moment from 'moment'
 
-export default ({
-    Vue, // VuePress 正在使用的 Vue 构造函数
-    options, // 附加到根实例的一些选项
-    router, // 当前应用的路由实例
-    siteData // 站点元数据
-  }) => {
-    Vue.use(ElementUI);
+const dataMixin = {
+  computed: {
+    $posts () {
+      const pages = this.$site.pages
+      const pageFilter = p => {
+        return p.frontmatter.layout === 'post'
+      }
+      const pageSort = (p1, p2) => {
+        return p1.frontmatter.date < p2.frontmatter.date
+      }
+      const pageMap = p => {
+        p.createdAt = moment(p.frontmatter.date).format('YYYY-MM-DD')
+        p.updatedAt = Boolean(p.lastUpdated) ? moment(p.lastUpdated).format('YYYY-MM-DD') : null
+        p.tags = p.frontmatter.tags || []
+        p.category = p.frontmatter.category || null
+        return p
+      }
+      const posts = pages.filter(pageFilter).sort(pageSort).map(pageMap)
+      return posts
+    },
+    $categories () {
+      let categoriesSet = new Set()
+      for (const post of this.$posts) {
+        if (post.category) {
+          categoriesSet.add(post.category)
+        }
+      }
+      return Array.from(categoriesSet)
+    },
+    $tags () {
+      let tagsArr = []
+      for (const post of this.$posts) {
+        tagsArr = tagsArr.concat(post.tags)
+      }
+      return Array.from(new Set(tagsArr))
+    }
   }
+}
+
+export default ({ Vue, options }) => {
+  Vue.mixin(dataMixin)
+
+  Vue.component('FontAwesomeIcon', {
+    functional: true,
+    props: FontAwesomeIcon.props,
+    render (h, context) {
+      if (context.parent._isMounted) {
+        return h(FontAwesomeIcon, context)
+      } else {
+        context.parent.$once('hook:mounted', () => {
+          context.parent.$forceUpdate()
+        })
+      }
+    }
+  })
+}

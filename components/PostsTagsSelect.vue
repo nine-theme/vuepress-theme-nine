ebdaffc12baa846f0a0d869443d744ff88b95171
王佳 <wangjia_1919@163.com>
2019-05-21 23:30:08 +0800

    update some icon

---
+++ components/PostsTagsSelect.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tags-select">
+    <label
+      v-for="tag in $tags"
+      :key="tag"
+      class="tag-checkbox"
+    >
+      <input
+        v-show="false"
+        type="checkbox"
+        :value="tag"
+        v-model="selectedTags"
+      >
+
+      <IconTag :name="tag" />
+    </label>
+  </div>
+</template>
+
+<script>
+import IconTag from '@theme/components/widgets/IconTag'
+
+export default {
+  name: 'PostsTagsSelect',
+
+  components: {
+    IconTag,
+  },
+
+  data () {
+    return {
+      selectedTags: [],
+    }
+  },
+
+  watch: {
+    selectedTags (val) {
+      this.$emit('input', val)
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+@require '~@theme/styles/variables'
+
+.tag-checkbox
+  &:not(:first-child)
+    margin-left 0.3rem
+  input[type="checkbox"]:checked + .post-tag
+    color $accentColor
+    font-weight bold
+
+</style>
---
+++ components/widgets/IconSns.vue
@@ -0,0 +1,115 @@
+<template>
+  <span
+    v-if="icon"
+    class="sns-icon"
+    :title="title"
+  >
+    <FontAwesomeIcon
+      :icon="icon"
+      :size="size"
+      :fixed-width="fixedWidth"
+      v-bind="$attrs"
+    />
+  </span>
+</template>
+
+<script>
+import {
+  faGithub,
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+  faWeibo,
+  faZhihu,
+} from '@fortawesome/free-brands-svg-icons'
+
+const nameMap = {
+  'github': {
+    title: 'GitHub',
+    icon: faGithub,
+  },
+  'facebook': {
+    title: 'Facebook',
+    icon: faFacebook,
+  },
+  'twitter': {
+    title: 'Twitter',
+    icon: faTwitter,
+  },
+  'linkedin': {
+    title: 'LinkedIn',
+    icon: faLinkedin,
+  },
+  'weibo': {
+    title: 'WeiBo',
+    icon: faWeibo,
+  },
+  'zhihu': {
+    title: 'ZhiHu',
+    icon: faZhihu,
+  },
+  'douban': {
+    title: 'DouBan',
+    icon: null,
+  },
+}
+
+export default {
+  name: 'IconSns',
+
+  props: {
+    // Account of SNS
+    account: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
+    // Name of SNS
+    name: {
+      type: String,
+      required: true,
+    },
+
+    // Size of FontAwesome
+    size: {
+      type: String,
+      required: false,
+      default: 'lg',
+    },
+
+    // Fixed Width or not of FontAwesome
+    fixedWidth: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+  },
+
+  computed: {
+
+    data () {
+      return nameMap[this.name]
+    },
+
+    title () {
+      return `${this.data.title}: ${this.account}`
+    },
+
+    icon () {
+      return this.data.icon || false
+    },
+  },
+}
+</script>
+
+<style lang="stylus">
+@require '~@theme/styles/variables'
+
+.sns-link
+  .sns-icon
+    color $grayTextColor
+    transition all 0.5s ease-out
+    &:hover
+      color $accentColor
+</style>

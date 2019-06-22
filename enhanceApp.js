fb13f50c5607a53b251132f4200053bf198c0c50
王佳 <wangjia_1919@163.com>
2019-06-22 12:30:08 +0800

    add iview

--- enhanceApp.js
+++ enhanceApp.js
@@ -1,8 +1,18 @@
+import iview from 'iview';
+import './styles/index.less';
+
 export default ({
     Vue, // VuePress 正在使用的 Vue 构造函数
     options, // 附加到根实例的一些选项
     router, // 当前应用的路由实例
     siteData // 站点元数据
   }) => {
-    // ...做一些其他的应用级别的优化
+    Vue.use(iview, {
+        transfer: true,
+        size: 'default',
+        select: {
+            arrow: 'md-arrow-dropdown',
+            arrowSize: 20
+        }
+    })
   }
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-export const plugins = [
-    '@vuepress/pwa',
-    {
-        serviceWorker: true,
-        updatePopup: true
+module.exports = (themeConfig, ctx) => {
+    return {
+        plugins: [
+            '@vuepress/pwa',
+            {
+                serviceWorker: true,
+                updatePopup: true
+            }
+        ],
+        chainWebpack: config => {
+            config.module
+                .rule('less')
+                .test(/\.less$/)
+                .use('less-loader')
+                .loader('less-loader')
+                .tap(options => {
+                    return {
+                        javascriptEnabled: true
+                    }
+                }).end()
+        }
     }
-];
+}

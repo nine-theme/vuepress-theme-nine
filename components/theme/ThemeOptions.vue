2e4ace4c39a726661a3507912a413fc5c4eddaf6
抱紧我的小鲤鱼 <wangjia_1919@163.com>
2019-12-02 13:21:14 +0800

    fix theme

--- components/theme/ThemeOptions.vue
+++ components/theme/ThemeOptions.vue
@@ -1,122 +1,119 @@
-<template>
-	<div class="theme-options">
-		<ul v-if="nine.hasThemes" class="color-theme-options">
-			<li>
-				<a href="#" class="default-theme" @click.prevent="setTheme()"></a>
-			</li>
-			<li v-for="color in nine.themes" :key="color">
-				<a href="#" :class="`${color}-theme`" :style="{background: colors[color]}" @click.prevent="setTheme(color)"></a>
-			</li>
-		</ul>
-		<div v-if="!nine.disableDarkTheme" class="dark-theme-options toggle-option">
-			<label for="dark-theme-toggle">Enable Dark Theme?</label>
-			<input id="dark-theme-toggle" v-model="darkTheme" type="checkbox" @change="toggleDarkTheme" />
-		</div>
-	</div>
+<template lang="pug">
+  div(class="theme-options")
+    ul(class="color-theme-options")
+      li
+        a(@click.prevent="setTheme()" class="default-theme" href="#")
+      li(v-for="(value, key) in themePicker" :key="key")
+        a(href="#" :class="`${key}-theme`" :style="{background: value}" @click.prevent="setTheme(key)")
+    div(class="dark-theme-options toggle-option")
+      label(for="dark-theme-toggle") Enable Dark Theme?
+      input(@change="toggleDarkTheme" id="dark-theme-toggle" type="checkbox" v-model="darkTheme")
 </template>
 
 <script>
-import recoConfig from './nineConfig.js';
+  export default {
+    name: 'ThemeOptions',
+    data() {
+      return {
+        darkTheme: 'false',
+        nine: {}
+      };
+    },
 
-export default {
-	name: 'ThemeOptions',
+    computed: {
+      themePicker () {
+        return this.$themeConfig.themePicker || {
+          red: '#f26d6d',
+          blue: '#2196f3',
+          green: '#3eaf7c',
+          orange: '#fb9b5f'
+        }
+      }
+    },
 
-	mixins: [recoConfig],
+    mounted() {
+      const theme = localStorage.getItem('nine-theme')
+      const night = localStorage.getItem('nine-night')
+      const classes = document.body.classList;
 
-	data() {
-		return {
-			darkTheme: 'false'
-		};
-	},
+      this.darkTheme = night === 'true' ? true : false
+      if (night === 'true') classes.add(`nine-theme-night`)
+      if (theme) this.setTheme(theme)
+    },
 
-	mounted() {
-		const theme = localStorage.getItem('nine-theme')
-		const night = localStorage.getItem('nine-night')
-		const classes = document.body.classList;
+    methods: {
+      toggleDarkTheme() {
+        localStorage.setItem('nine-night', this.darkTheme)
+        const classes = document.body.classList;
+        if (this.darkTheme) {
+          const oldColor = [...classes]
+          classes.value = ''
+          classes.add(`nine-theme-night`)
+          oldColor.forEach(item => {
+            classes.add(item)
+          })
+        } else {
+          classes.remove(`nine-theme-night`)
+        }
+      },
+      setTheme(theme, moveClass = true) {
+        const classes = document.body.classList
+        const themes = Object.keys(this.themePicker).map(colorTheme => `reco-theme-${colorTheme}`)
 
-		this.darkTheme = night === 'true' ? true : false
-		if (night === 'true') classes.add(`nine-theme-night`)
-		if (theme) this.setTheme(theme)
-	},
+        if (!theme) {
+          if (moveClass) localStorage.removeItem('nine-theme')
+          classes.remove(...themes);
+          return
+        }
 
-	methods: {
-		toggleDarkTheme() {
-			localStorage.setItem('nine-night', this.darkTheme)
-			const classes = document.body.classList;
-			if (this.darkTheme) {
-				const oldColor = [...classes]
-				classes.value = ''
-				classes.add(`nine-theme-night`)
-				oldColor.forEach(item => {
-					classes.add(item)
-				})
-			}
-			else {
-				classes.remove(`nine-theme-night`)
-			}
-		},
-		setTheme(theme, moveClass = true) {
-			const colorThemes = this.nine.themes;
+        classes.remove(...themes.filter(t => t !== `nine-theme-${theme}`))
 
-			const classes = document.body.classList;
-			const themes = colorThemes.map(colorTheme => `nine-theme-${colorTheme}`);
-
-			if (!theme) {
-				if (moveClass) localStorage.removeItem('nine-theme');
-				classes.remove(...themes);
-				return
-			}
-
-			classes.remove(...themes.filter(t => t !== `nine-theme-${theme}`));
-
-			if (moveClass) {
-				classes.add(`nine-theme-${theme}`);
-				localStorage.setItem('nine-theme', theme);
-			} else {
-				localStorage.removeItem('nine-theme')
-				classes.remove(`nine-theme-${theme}`);
-			}
-		}
-	},
-};
+        if (moveClass) {
+          classes.add(`nine-theme-${theme}`);
+          localStorage.setItem('nine-theme', theme);
+        } else {
+          localStorage.removeItem('nine-theme')
+          classes.remove(`nine-theme-${theme}`);
+        }
+      }
+    },
+  };
 </script>
 
 <style lang="stylus">
 
-.color-theme-options {
-	display: flex;
-	justify-content: space-around;
-	li {
-		width: 33%;
-		text-align: center;
+  .color-theme-options {
+    display: flex;
+    flex-wrap wrap;
 
-		a {
-			width: 15px;
-			height: 15px;
-			border-radius: 2px;
+    li {
+      width: 20%;
+      text-align: center;
 
-			&.default-theme {
-				background-color: $accentColor;
-			}
+      a {
+        width: 15px;
+        height: 15px;
+        border-radius: 2px;
 
-			&.blue-theme {
-				background-color: $blueAccentColor;
-			}
+        &.default-theme {
+          background-color: $accentColor;
+        }
+        for key, value in $themePicker {
+          &.{key}-theme {
+            background-color: value;
+          }
+        }
+      }
+    }
+  }
 
-			&.red-theme {
-				background-color: $redAccentColor;
-			}
-		}
-	}
-}
+  .toggle-option {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-.toggle-option {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	label {
-		padding-right: 0.25em;
-	}
-}
+    label {
+      padding-right: 0.25em;
+    }
+  }
 </style>
